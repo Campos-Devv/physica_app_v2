f9a30e5b460e6628bf82f74d05145998b4dee310
Campos-Devv <campos.devv@gmail.com>
2025-07-03 23:50:52 +0800

    add firebase options

--- lib/firebase/firebase_options.dart
+++ lib/firebase/firebase_options.dart
@@ -1,8 +1,61 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart'
+    show defaultTargetPlatform, kIsWeb, TargetPlatform;
 
-// ...
+/// Default [FirebaseOptions] for use with your Firebase apps.
+///
+/// Example:
+/// ```dart
+/// import 'firebase_options.dart';
+/// // ...
+/// await Firebase.initializeApp(
+///   options: DefaultFirebaseOptions.currentPlatform,
+/// );
+/// ```
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    if (kIsWeb) {
+      throw UnsupportedError(
+        'DefaultFirebaseOptions have not been configured for web - '
+        'you can create these using the FlutterFire CLI.',
+      );
+    }
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return android;
+      case TargetPlatform.iOS:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for ios - '
+          'you can create these using the FlutterFire CLI.',
+        );
+      case TargetPlatform.macOS:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for macos - '
+          'you can create these using the FlutterFire CLI.',
+        );
+      case TargetPlatform.windows:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for windows - '
+          'you can create these using the FlutterFire CLI.',
+        );
+      case TargetPlatform.linux:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for linux - '
+          'you can create these using the FlutterFire CLI.',
+        );
+      default:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions are not supported for this platform.',
+        );
+    }
+  }
 
-await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-);
+  // Replace these values with the ones from your google-services.json file
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'AIzaSyAZCZ7gzSk5-fcqMvT0kaQZk27JVYKhWWc',
+    appId: '1:441474208790:android:9115a78e48f5f3280f1137',
+    messagingSenderId: '441474208790',
+    projectId: 'physica-app-c718b',
+    storageBucket: 'physica-app-c718b.firebasestorage.app',
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:physica_app/screens/sign_in.dart';
 import 'package:physica_app/screens/splash_screen.dart';
+import 'package:physica_app/firebase_options.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     const MyApp(),
   );
